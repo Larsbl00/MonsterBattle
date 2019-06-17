545801bdbf3ed99154a69e58e27a2e86930d2003
Larsbl00 <30385165+Larsbl00@users.noreply.github.com>
2019-06-17 13:40:35 +0100

    Added the readers delegate

---
+++ include/InputDelegate.h
@@ -0,0 +1,22 @@
+/**
+ * @file InputDelegate.h
+ * @author Lars Bloemers
+ * @brief 
+ * @version 0.1
+ * @date 2019-06-17
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
+#ifndef INPUT_DELEGATE_H
+#define INPUT_DELEGATE_H
+
+struct InputDelegate
+{
+    virtual ~InputDelegate(){}
+
+    virtual void onKeyPress(char* input) = 0;
+};
+
+#endif
---
+++ include/InputReader.h
@@ -0,0 +1,24 @@
+/**
+ * @file InputReader.h
+ * @author Lars Bloemers
+ * @brief 
+ * @version 0.1
+ * @date 2019-06-17
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
+#ifndef INPUT_READER_H
+#define INPUT_READER_H
+
+class InputReader
+{
+    public:
+
+
+    private:
+    
+};
+
+#endif
