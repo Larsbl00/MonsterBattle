c88ddf92155423f4414743660b317fe081c27422
Larsbl00 <30385165+Larsbl00@users.noreply.github.com>
2019-06-20 13:58:48 +0200

    Added types

---
+++ include/Type.h
@@ -0,0 +1,26 @@
+/**
+ * @file Types.h
+ * @author Lars Bloemers
+ * @brief 
+ * @version 0.1
+ * @date 2019-06-20
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
+#ifndef TYPE_H
+#define TYPE_H
+
+enum class Type
+{
+    NONE,
+    FIRE,
+    WATER,
+    GRASS,
+    ROCK,
+    FLYING,
+    ICE
+};
+
+#endif
