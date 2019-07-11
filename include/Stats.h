2574388ce3a32dfd49137d93dbf381d3679040da
Larsbl00 <larsbloemers@gmail.com>
2019-07-11 10:25:44 +0200

    Added constructors for the stats

--- include/Stats.h
+++ include/Stats.h
@@ -20,12 +20,20 @@ namespace monsterbattle
     {
         struct Stats
         {
+            Stats(uint8_t health, uint8_t defense, uint8_t attack, float precision, float avoidance, uint8_t speed):
+                maxHealth(health), health(health), defense(defense), attack(attack), 
+                precision(precision), avoidance(avoidance), speed(speed)
+            {}
+
+            Stats(const Stats& other) = default;
+            ~Stats() noexcept = default;
+
             const uint8_t maxHealth;    //Maximum health stat
             uint8_t health;             //Health stat
             uint8_t defense;            //Defense stat
             uint8_t attack;             //Attack stat
             float precision;            //Precision, a normalized value showing the accuracy
-            float avoidance;            //Avoidancem a normalized value displaying the probability of avoiding an attack
+            float avoidance;            //Avoidance, a normalized value displaying the probability of avoiding an attack
             uint8_t speed;              //Speed stat
         };
     }  
